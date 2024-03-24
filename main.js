644be194547896a890dc7c23cfcac9959b5a7bb1
Cindel Guerrero D'Agostini <cindel.guerrerod@ufl.edu>
2024-03-24 14:50:04 -0400

    input and output SVGs

--- main.js
+++ main.js
@@ -49,8 +49,7 @@ var main=function(args){
     connectorSignal.setIcon(new GUIIcon(SVGICONS_DICT["connectorIcon"]));
     var outputSignal=gatesAndSignals.append(new Button(""));
     outputSignal.setToolTipText("Output Signal");
-    //output Icon doesn't work, need to figure out how to flip svg icon
-    outputSignal.setIcon(new GUIIcon(SVGICONS_DICT["inputIcon"]));
+    outputSignal.setIcon(new GUIIcon(SVGICONS_DICT["outputIcon"]));
 
 
     vertSplitLayout.getFirstContainer().append(gatesAndSignals);
