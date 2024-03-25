5b504ee208680e9d6c7f0007ab5f162c3db519ce
catherinepham <cathy.02pham@gmail.com>
2024-03-24 21:13:38 -0400

    Add button when clicked functionality
    still need to figure out how to init the gate classes and store the svg in there instead

--- main.js
+++ main.js
@@ -67,16 +67,64 @@ var main=function(args){
     //button on-click functionality
     andGate.whenClicked().then(
         (button) => {
-            //button.setLabel("Clicked");
-            // const andGateSVG = `<svg xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" viewBox="0 0 12.69922 15.874025" version="1.1" x="0px" y="0px"><g transform="translate(-123.27327,-135.85802)"><path style="color:#000000;" d="m 127.875,138.91602 c -0.4777,-1e-5 -0.87305,0.39535 -0.87305,0.87304 v 4.83594 c 0,0.4777 0.39535,0.875 0.87305,0.875 h 1.63867 c 1.81301,0 3.29297,-1.47974 3.29297,-3.29297 0,-1.81322 -1.47974,-3.29101 -3.29297,-3.29101 z m 0,0.79296 h 1.63867 c 1.38425,0 2.49805,1.1138 2.49805,2.49805 0,1.38427 -1.11408,2.49805 -2.49805,2.49805 H 127.875 c -0.0487,0 -0.0801,-0.0314 -0.0801,-0.0801 v -4.83594 c 0,-0.0487 0.0314,-0.0801 0.0801,-0.0801 z" fill="#000000" stroke-linecap="round" stroke-linejoin="round"/><path style="color:#000000;" d="m 132.4082,141.81055 a 0.3968755,0.3968755 0 0 0 -0.39648,0.39648 0.3968755,0.3968755 0 0 0 0.39648,0.39649 h 2.12696 a 0.3968755,0.3968755 0 0 0 0.39648,-0.39649 0.3968755,0.3968755 0 0 0 -0.39648,-0.39648 z" fill="#000000" stroke-linecap="round" stroke-linejoin="round"/><path style="color:#000000;" d="m 124.73438,143.25586 a 0.3968755,0.3968755 0 0 0 -0.39844,0.39648 0.3968755,0.3968755 0 0 0 0.39844,0.39844 h 2.66406 a 0.3968755,0.3968755 0 0 0 0.39648,-0.39844 0.3968755,0.3968755 0 0 0 -0.39648,-0.39648 z" fill="#000000" stroke-linecap="round" stroke-linejoin="round"/><path style="color:#000000;" d="m 124.73047,140.36523 a 0.3968755,0.3968755 0 0 0 -0.39844,0.39649 0.3968755,0.3968755 0 0 0 0.39844,0.39648 h 2.66797 a 0.3968755,0.3968755 0 0 0 0.39648,-0.39648 0.3968755,0.3968755 0 0 0 -0.39648,-0.39649 z" fill="#000000" stroke-linecap="round" stroke-linejoin="round"/></g><text x="0" y="27.69922" fill="#000000" font-size="5px" font-weight="bold" font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">Created by Meko</text><text x="0" y="32.69922" fill="#000000" font-size="5px" font-weight="bold" font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">from the Noun Project</text></svg>`;
-            // const andGate1 = new ANDGate(andGateSVG, false);
-            mySVG.rect({y:50,x:50,width:50,height:50,stroke:'#000000',fill:"rgba(0,0,0,0)",strokeWidth:"3"},{canResize:false})
+            // init and gate class
+            mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-and-gate-5536388.svg",preserveAspectRatio:"none"});
         }
     );
-
-    //mySVG.text({x:30,y:100,text:"Hello this is my test",'font-family':'Arial','font-size':'20px',fill:'black',style:'user-select:none;'})
-    // mySVG.ellipse({cy:250,cx:250,rx:209,ry:20,stroke:'#000000',fill:"rgba(0,0,0,0)",strokeWidth:"3"})
-	// mySVG .image({y:400,x:400,width:200,height:200,href:"https://digitalworlds.github.io/op.n/img/logo.png",preserveAspectRatio:"none"});
+    orGate.whenClicked().then(
+        (button) => {
+            // init gate class
+            mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-or-gate-5536390.svg",preserveAspectRatio:"none"});
+        }
+    );
+    notGate.whenClicked().then(
+        (button) => {
+            // init gate class
+            mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-not-gate-5536385.svg",preserveAspectRatio:"none"});
+        }
+    );
+    xorGate.whenClicked().then(
+        (button) => {
+            // init gate class
+            mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-xor-gate-5536387.svg",preserveAspectRatio:"none"});
+        }
+    );
+    nandGate.whenClicked().then(
+        (button) => {
+            // init gate class
+            mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-nand-gate-1375339.svg",preserveAspectRatio:"none"});
+        }
+    );
+    norGate.whenClicked().then(
+        (button) => {
+            // init gate class
+            mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-nor-77568.svg",preserveAspectRatio:"none"});
+        }
+    );
+    xnorGate.whenClicked().then(
+        (button) => {
+            // init gate class
+            mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-xnor-gate-3834994.svg",preserveAspectRatio:"none"});
+        }
+    );
+    inputSignal.whenClicked().then(
+        (button) => {
+            // init gate class
+            mySVG.image({y:50,x:50,width:50,height:50,href:"gates/line-with-dot.svg",preserveAspectRatio:"none"});
+        }
+    );
+    connectorSignal.whenClicked().then(
+        (button) => {
+            // init gate class
+            mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-line-924522.svg",preserveAspectRatio:"none"});
+        }
+    );
+    outputSignal.whenClicked().then(
+        (button) => {
+            // init gate class
+            mySVG.image({y:50,x:50,width:50,height:50,href:"gates/flipped-line-with-dot.svg",preserveAspectRatio:"none"});
+        }
+    );
 
     div.appendChild(mySVG.svg);
     
