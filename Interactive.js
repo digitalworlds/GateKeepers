b00354afeb549b094314ce9cb8cc71bf92adf425
eeshashah <73673633+eeshashah@users.noreply.github.com>
2024-04-03 11:13:39 -0400

    some changes

--- Interactive.js
+++ Interactive.js
@@ -1,227 +1,231 @@
 class InteractiveSVG {
-    constructor() {
-      if (new.target === InteractiveSVG) {
-        throw new TypeError("Cannot construct InteractiveSVG instances directly");
-      }
-      // SVG container setup for interactive elements.
-      this.svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-      this.svgElement.setAttribute("width", "100px"); // Set appropriate size
-      this.svgElement.setAttribute("height", "50px"); // Set appropriate size
+  constructor() {
+    if (new.target === InteractiveSVG) {
+      throw new TypeError("Cannot construct InteractiveSVG instances directly");
     }
-  
-    onClick(listener) {
-      this.svgElement.addEventListener('click', listener);
-    }
-  
-    setSVGContent(svgContent) {
-      this.svgElement.innerHTML = svgContent;
-    }
-  
-    setPosition(x, y) {
-      this.svgElement.style.transform = `translate(${x}px, ${y}px)`;
-    }
-  
-    // Method to make the SVG draggable within the container
-    makeDraggable() {
-      let drag = false;
-      let offsetX, offsetY;
-  
-      this.svgElement.addEventListener('mousedown', (event) => {
-        drag = true;
-        offsetX = event.clientX - this.svgElement.getBoundingClientRect().left;
-        offsetY = event.clientY - this.svgElement.getBoundingClientRect().top;
-        this.svgElement.style.cursor = 'grabbing';
-      });
-  
-      document.addEventListener('mousemove', (event) => {
-        if (drag) {
-          this.setPosition(event.clientX - offsetX, event.clientY - offsetY);
-        }
-      });
-  
-      document.addEventListener('mouseup', () => {
-        drag = false;
-        this.svgElement.style.cursor = 'grab';
-      });
-    }
-  
-    // Add any other methods to manipulate the SVG as needed
+    // SVG container setup for interactive elements.
+    this.svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    this.svgElement.setAttribute("width", "100px"); // Set appropriate size
+    this.svgElement.setAttribute("height", "50px"); // Set appropriate size
   }
-  
-  
-class Signal extends InteractiveSVG {
-    constructor(svgURL) {
-      super(svgURL);
-      this.value = false;
-      this.listeners = []; // Gates that listen to this signal
-    }
-  
-    setValue(value) {
-      if (this.value !== value) {
-        this.value = value;
-        // Notify all listeners (gates) that the value has changed.
-        this.listeners.forEach(listener => listener());
-        // Optionally, update the SVG visual representation based on the signal value.
-      }
-    }
-  
-    addListener(listener) {
-      this.listeners.push(listener);
-    }
-  }
-  
-  class ConnectorSignal extends Signal {
-    constructor(svgURL) {
-      super(svgURL);
-    }
-  
-    connect(inputGate, outputGate) {
-      inputGate.setOutput(this);
-      outputGate.setInput(this);
-      this.addListener(() => outputGate.calculateOutput());
-    }
-  }
-  
-  class InputSignal extends Signal {
-    constructor(svgURL) {
-      super(svgURL);
-    }
-  
-    setUserValue(value) {
-      // Method for user to set the initial signal value
-      this.setValue(value);
-    }
-  }
-  
-  class OutputSignal extends Signal {
-    constructor(svgURL) {
-      super(svgURL);
-    }
-  
-    reportValue() {
-      // Method to report the boolean value/output to the user
-      console.log('Output value:', this.value);
-      // Additionally, update the SVG visual representation or UI element that displays the value
-    }
-  }
-  
-class Gate extends InteractiveSVG {
-    constructor(svgURL) {
-      super(svgURL);
-      this.firstInputSignal = null;
-      this.secondInputSignal = null;
-      this.outputSignal = null;
-    }
-  
-    setInput(inputSignal) {
-      if (!this.firstInputSignal) {
-        this.firstInputSignal = inputSignal;
-      } else if (!this.secondInputSignal) {
-        this.secondInputSignal = inputSignal;
-      } else {
-        throw new Error("Both inputs already connected");
-      }
-      inputSignal.addListener(() => this.calculateOutput());
-    }
-  
-    setOutput(outputSignal) {
-      this.outputSignal = outputSignal;
-    }
-  
-    calculateOutput() {
-      throw new Error("Must override calculateOutput in subclass");
-    }
-  }
-  
-  class ANDGate extends Gate {
-    constructor(svgContent, isNAND) {
-      super();
-      this.setSVGContent(svgContent);
-      this.NAND = isNAND;
-    }
 
-  calculateOutput() {
-      if (this.firstInputSignal && this.secondInputSignal) {
-        const outputValue = this.firstInputSignal.value && this.secondInputSignal.value;
-        if(!NAND) {
-          this.outputSignal.setValue(outputValue);
-        } else {
-          this.outputSignal.setValue(!outputValue);
-        }
+  onClick(listener) {
+    this.svgElement.addEventListener('click', listener);
+  }
+
+  setSVGContent(svgContent) {
+    this.svgElement.innerHTML = svgContent;
+  }
+
+  getSVGContent() {
+    return this.svgElement.innerHTML;
+  }
+
+  setPosition(x, y) {
+    this.svgElement.style.transform = `translate(${x}px, ${y}px)`;
+  }
+
+  // Method to make the SVG draggable within the container
+  makeDraggable() {
+    let drag = false;
+    let offsetX, offsetY;
+
+    this.svgElement.addEventListener('mousedown', (event) => {
+      drag = true;
+      offsetX = event.clientX - this.svgElement.getBoundingClientRect().left;
+      offsetY = event.clientY - this.svgElement.getBoundingClientRect().top;
+      this.svgElement.style.cursor = 'grabbing';
+    });
+
+    document.addEventListener('mousemove', (event) => {
+      if (drag) {
+        this.setPosition(event.clientX - offsetX, event.clientY - offsetY);
       }
+    });
+
+    document.addEventListener('mouseup', () => {
+      drag = false;
+      this.svgElement.style.cursor = 'grab';
+    });
   }
-  }
-  
-class ORGate extends Gate {
-  constructor(svgContent, isNOR) {
-    super();
-    this.setSVGContent(svgContent);
-    this.NOR = isNOR;
-  }
-    calculateOutput() {
-      if (this.firstInputSignal && this.secondInputSignal) {
-        const outputValue = this.firstInputSignal.value || this.secondInputSignal.value;
-        if(!NOR) {
-          this.outputSignal.setValue(outputValue);
-        } else {
-          this.outputSignal.setValue(!outputValue);
-        }
-      }
-    }
-  }
-  
-class NOTGate extends Gate {
-  constructor(svgContent) {
-    super();
-    this.setSVGContent(svgContent);
-  }
-    calculateOutput() {
-      if (this.firstInputSignal) {
-        const outputValue = !this.firstInputSignal.value;
-        this.outputSignal.setValue(outputValue);
-      }
-    }
-  }
-  
-  class XORGate extends Gate {
-    constructor(svgContent, isXNOR) {
-      super();
-      this.setSVGContent(svgContent);
-      this.XNOR = isXNOR;
-    }
-    calculateOutput() {
-      if (this.firstInputSignal && this.secondInputSignal) {
-        const outputValue = (this.firstInputSignal.value !== this.secondInputSignal.value);
-        if(!XNOR) {
-          this.outputSignal.setValue(outputValue);
-        } else {
-          this.outputSignal.setValue(!outputValue);
-        }
-      }
-    }
-  }
-  var main = function() {
-   // opn.extend(InteractiveSVG,ANDGate);
-  	exportData({InteractiveSVG});
+
+  // Add any other methods to manipulate the SVG as needed
 }
 
- /*
-  // Example usage:
+
+class Signal extends InteractiveSVG {
+  constructor(svgURL) {
+    super(svgURL);
+    this.value = false;
+    this.listeners = []; // Gates that listen to this signal
+  }
+
+  setValue(value) {
+    if (this.value !== value) {
+      this.value = value;
+      // Notify all listeners (gates) that the value has changed.
+      this.listeners.forEach(listener => listener());
+      // Optionally, update the SVG visual representation based on the signal value.
+    }
+  }
+
+  addListener(listener) {
+    this.listeners.push(listener);
+  }
+}
+
+class ConnectorSignal extends Signal {
+  constructor(svgURL) {
+    super(svgURL);
+  }
+
+  connect(inputGate, outputGate) {
+    inputGate.setOutput(this);
+    outputGate.setInput(this);
+    this.addListener(() => outputGate.calculateOutput());
+  }
+}
+
+class InputSignal extends Signal {
+  constructor(svgURL) {
+    super(svgURL);
+  }
+
+  setUserValue(value) {
+    // Method for user to set the initial signal value
+    this.setValue(value);
+  }
+}
+
+class OutputSignal extends Signal {
+  constructor(svgURL) {
+    super(svgURL);
+  }
+
+  reportValue() {
+    // Method to report the boolean value/output to the user
+    console.log('Output value:', this.value);
+    // Additionally, update the SVG visual representation or UI element that displays the value
+  }
+}
+
+class Gate extends InteractiveSVG {
+  constructor(svgURL) {
+    super(svgURL);
+    this.firstInputSignal = null;
+    this.secondInputSignal = null;
+    this.outputSignal = null;
+  }
+
+  setInput(inputSignal) {
+    if (!this.firstInputSignal) {
+      this.firstInputSignal = inputSignal;
+    } else if (!this.secondInputSignal) {
+      this.secondInputSignal = inputSignal;
+    } else {
+      throw new Error("Both inputs already connected");
+    }
+    inputSignal.addListener(() => this.calculateOutput());
+  }
+
+  setOutput(outputSignal) {
+    this.outputSignal = outputSignal;
+  }
+
+  calculateOutput() {
+    throw new Error("Must override calculateOutput in subclass");
+  }
+}
+
+class ANDGate extends Gate {
+  constructor(svgContent, isNAND) {
+    super();
+    this.setSVGContent(svgContent);
+    this.NAND = isNAND;
+  }
+
+  calculateOutput() {
+    if (this.firstInputSignal && this.secondInputSignal) {
+      const outputValue = this.firstInputSignal.value && this.secondInputSignal.value;
+      if(!NAND) {
+        this.outputSignal.setValue(outputValue);
+      } else {
+        this.outputSignal.setValue(!outputValue);
+      }
+    }
+  }
+}
+
+class ORGate extends Gate {
+constructor(svgContent, isNOR) {
+  super();
+  this.setSVGContent(svgContent);
+  this.NOR = isNOR;
+}
+  calculateOutput() {
+    if (this.firstInputSignal && this.secondInputSignal) {
+      const outputValue = this.firstInputSignal.value || this.secondInputSignal.value;
+      if(!NOR) {
+        this.outputSignal.setValue(outputValue);
+      } else {
+        this.outputSignal.setValue(!outputValue);
+      }
+    }
+  }
+}
+
+class NOTGate extends Gate {
+constructor(svgContent) {
+  super();
+  this.setSVGContent(svgContent);
+}
+  calculateOutput() {
+    if (this.firstInputSignal) {
+      const outputValue = !this.firstInputSignal.value;
+      this.outputSignal.setValue(outputValue);
+    }
+  }
+}
+
+class XORGate extends Gate {
+  constructor(svgContent, isXNOR) {
+    super();
+    this.setSVGContent(svgContent);
+    this.XNOR = isXNOR;
+  }
+  calculateOutput() {
+    if (this.firstInputSignal && this.secondInputSignal) {
+      const outputValue = (this.firstInputSignal.value !== this.secondInputSignal.value);
+      if(!XNOR) {
+        this.outputSignal.setValue(outputValue);
+      } else {
+        this.outputSignal.setValue(!outputValue);
+      }
+    }
+  }
+}
+var main = function() {
+ // opn.extend(InteractiveSVG,ANDGate);
+  exportData({InteractiveSVG});
+}
+
+/*
+// Example usage:
 const andGateSVG = `<svg xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" viewBox="0 0 12.69922 15.874025" version="1.1" x="0px" y="0px"><g transform="translate(-123.27327,-135.85802)"><path style="color:#000000;" d="m 127.875,138.91602 c -0.4777,-1e-5 -0.87305,0.39535 -0.87305,0.87304 v 4.83594 c 0,0.4777 0.39535,0.875 0.87305,0.875 h 1.63867 c 1.81301,0 3.29297,-1.47974 3.29297,-3.29297 0,-1.81322 -1.47974,-3.29101 -3.29297,-3.29101 z m 0,0.79296 h 1.63867 c 1.38425,0 2.49805,1.1138 2.49805,2.49805 0,1.38427 -1.11408,2.49805 -2.49805,2.49805 H 127.875 c -0.0487,0 -0.0801,-0.0314 -0.0801,-0.0801 v -4.83594 c 0,-0.0487 0.0314,-0.0801 0.0801,-0.0801 z" fill="#000000" stroke-linecap="round" stroke-linejoin="round"/><path style="color:#000000;" d="m 132.4082,141.81055 a 0.3968755,0.3968755 0 0 0 -0.39648,0.39648 0.3968755,0.3968755 0 0 0 0.39648,0.39649 h 2.12696 a 0.3968755,0.3968755 0 0 0 0.39648,-0.39649 0.3968755,0.3968755 0 0 0 -0.39648,-0.39648 z" fill="#000000" stroke-linecap="round" stroke-linejoin="round"/><path style="color:#000000;" d="m 124.73438,143.25586 a 0.3968755,0.3968755 0 0 0 -0.39844,0.39648 0.3968755,0.3968755 0 0 0 0.39844,0.39844 h 2.66406 a 0.3968755,0.3968755 0 0 0 0.39648,-0.39844 0.3968755,0.3968755 0 0 0 -0.39648,-0.39648 z" fill="#000000" stroke-linecap="round" stroke-linejoin="round"/><path style="color:#000000;" d="m 124.73047,140.36523 a 0.3968755,0.3968755 0 0 0 -0.39844,0.39649 0.3968755,0.3968755 0 0 0 0.39844,0.39648 h 2.66797 a 0.3968755,0.3968755 0 0 0 0.39648,-0.39648 0.3968755,0.3968755 0 0 0 -0.39648,-0.39649 z" fill="#000000" stroke-linecap="round" stroke-linejoin="round"/></g><text x="0" y="27.69922" fill="#000000" font-size="5px" font-weight="bold" font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">Created by Meko</text><text x="0" y="32.69922" fill="#000000" font-size="5px" font-weight="bold" font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">from the Noun Project</text></svg>`;
 const andGate = new ANDGate(andGateSVG, false);
 
 const inputSignal = new InputSignal('inputSignal.svg');
 const outputSignal = new OutputSignal('outputSignal.svg');
-  
+
 // User sets initial value of the input signal.
-  inputSignal.setUserValue(true);
-  
-  // Connect the input signal to the AND gate.
-  andGate.setInput(inputSignal);
-  
-  // Connect the AND gate to the output signal.
-  andGate.setOutput(outputSignal);
-  
-  // Complete circuit run and report output.
-  outputSignal.reportValue();  // Should log the value of the output signal.
+inputSignal.setUserValue(true);
+
+// Connect the input signal to the AND gate.
+andGate.setInput(inputSignal);
+
+// Connect the AND gate to the output signal.
+andGate.setOutput(outputSignal);
+
+// Complete circuit run and report output.
+outputSignal.reportValue();  // Should log the value of the output signal.
 */
--- main.js
+++ main.js
@@ -1,93 +1,163 @@
 preload(libs['GUI']).before(function(args){
-		args.app.showLoading();
-	});
+    args.app.showLoading();
+});
 
 preload("SVGEditor.js");
 preload("SVGIcons.js");
+preload("Interactive.js");
 
 var main=function(args){
-	args.app.clearContents();
+args.app.clearContents();
 
-	var wind=args.app.getWindow();
+var wind=args.app.getWindow();
 
-	var menulayout=new MenuLayout();
+var menulayout=new MenuLayout();
 
-  let runMenu=menulayout.getMenuBar().append(new MenuItem('Run'));
-  let instructionsMenu=menulayout.getMenuBar().append(new MenuItem('Instructions')).getSubMenu();
-  instructionsMenu.append(new MenuItem("CircuitCanvas: How to Build Your Own Collaborative Circuit.<br>To begin, hit insert to drag and drop elements into your circuit.<br>When you have finished building hit run to see it in action! "));
+let runMenu=menulayout.getMenuBar().append(new MenuItem('Run'));
+let instructionsMenu=menulayout.getMenuBar().append(new MenuItem('Instructions')).getSubMenu();
+instructionsMenu.append(new MenuItem("CircuitCanvas: How to Build Your Own Collaborative Circuit.<br>To begin, hit insert to drag and drop elements into your circuit.<br>When you have finished building hit run to see it in action! "));
 
+wind.getContent().append(menulayout);
 
-	wind.getContent().append(menulayout);
+//var vertSplitLayout=new SplitLayout({orientation:'vertical',sticky:'second',editable:false,splitPosition:'0.125'});
 
-    var vertSplitLayout=new SplitLayout({orientation:'vertical',sticky:'second',editable:false,splitPosition:'0.125'});
+var gatesAndSignals=new ButtonGroup({orientation:'horizontal'});
+var andGate=gatesAndSignals.append(new Button(""));
+andGate.setToolTipText("AND Gate");
+andGate.setIcon(new GUIIcon(SVGICONS_DICT["andGateIcon"]));
+var orGate=gatesAndSignals.append(new Button(""));
+orGate.setToolTipText("OR Gate");
+orGate.setIcon(new GUIIcon(SVGICONS_DICT["orGateIcon"]));
+var notGate=gatesAndSignals.append(new Button(""));
+notGate.setToolTipText("NOT Gate");
+notGate.setIcon(new GUIIcon(SVGICONS_DICT["notGateIcon"]));
+var xorGate=gatesAndSignals.append(new Button(""));
+xorGate.setToolTipText("XOR Gate");
+xorGate.setIcon(new GUIIcon(SVGICONS_DICT["xorGateIcon"]));
+var nandGate=gatesAndSignals.append(new Button(""));
+nandGate.setToolTipText("NAND Gate");
+nandGate.setIcon(new GUIIcon(SVGICONS_DICT["nandGateIcon"]));
+var norGate=gatesAndSignals.append(new Button(""));
+norGate.setToolTipText("NOR Gate");
+norGate.setIcon(new GUIIcon(SVGICONS_DICT["norGateIcon"]));
+var xnorGate=gatesAndSignals.append(new Button(""));
+xnorGate.setToolTipText("XNOR Gate");
+xnorGate.setIcon(new GUIIcon(SVGICONS_DICT["xnorGateIcon"]));
+var inputSignal=gatesAndSignals.append(new Button(""));
+inputSignal.setToolTipText("Input Signal");
+inputSignal.setIcon(new GUIIcon(SVGICONS_DICT["inputIcon"]));
+var connectorSignal=gatesAndSignals.append(new Button(""));
+connectorSignal.setToolTipText("Connector Signal");
+connectorSignal.setIcon(new GUIIcon(SVGICONS_DICT["connectorIcon"]));
+var outputSignal=gatesAndSignals.append(new Button(""));
+outputSignal.setToolTipText("Output Signal");
+outputSignal.setIcon(new GUIIcon(SVGICONS_DICT["outputIcon"]));
 
-    var gatesAndSignals=new ButtonGroup({orientation:'horizontal'});
-    var andGate=gatesAndSignals.append(new Button(""));
-    andGate.setToolTipText("AND Gate");
-    andGate.setIcon(new GUIIcon(SVGICONS_DICT["andGateIcon"]));
-    var orGate=gatesAndSignals.append(new Button(""));
-    orGate.setToolTipText("OR Gate");
-    orGate.setIcon(new GUIIcon(SVGICONS_DICT["orGateIcon"]));
-    var notGate=gatesAndSignals.append(new Button(""));
-    notGate.setToolTipText("NOT Gate");
-    notGate.setIcon(new GUIIcon(SVGICONS_DICT["notGateIcon"]));
-    var xorGate=gatesAndSignals.append(new Button(""));
-    xorGate.setToolTipText("XOR Gate");
-    xorGate.setIcon(new GUIIcon(SVGICONS_DICT["xorGateIcon"]));
-    var nandGate=gatesAndSignals.append(new Button(""));
-    nandGate.setToolTipText("NAND Gate");
-    nandGate.setIcon(new GUIIcon(SVGICONS_DICT["nandGateIcon"]));
-    var norGate=gatesAndSignals.append(new Button(""));
-    norGate.setToolTipText("NOR Gate");
-    norGate.setIcon(new GUIIcon(SVGICONS_DICT["norGateIcon"]));
-    var xnorGate=gatesAndSignals.append(new Button(""));
-    xnorGate.setToolTipText("XNOR Gate");
-    xnorGate.setIcon(new GUIIcon(SVGICONS_DICT["xnorGateIcon"]));
-    var inputSignal=gatesAndSignals.append(new Button(""));
-    inputSignal.setToolTipText("Input Signal");
-    inputSignal.setIcon(new GUIIcon(SVGICONS_DICT["inputIcon"]));
-    var connectorSignal=gatesAndSignals.append(new Button(""));
-    connectorSignal.setToolTipText("Connector Signal");
-    connectorSignal.setIcon(new GUIIcon(SVGICONS_DICT["connectorIcon"]));
-    var outputSignal=gatesAndSignals.append(new Button(""));
-    outputSignal.setToolTipText("Output Signal");
-    outputSignal.setIcon(new GUIIcon(SVGICONS_DICT["outputIcon"]));
+wind.append(gatesAndSignals);
 
+//menulayout.getContainer().append(vertSplitLayout);
+//var splitLayout=new SplitLayout({orientation:'horizontal',sticky:'second',editable:true,splitPosition:'0.25'});
+//vertSplitLayout.getSecondContainer().append(splitLayout);
 
-    vertSplitLayout.getFirstContainer().append(gatesAndSignals);
+var div=document.createElement("div");
+menulayout.getContainer().div.appendChild(div);
 
-    menulayout.getContainer().append(vertSplitLayout);
-    var splitLayout=new SplitLayout({orientation:'horizontal',sticky:'second',editable:true,splitPosition:'0.25'});
-    vertSplitLayout.getSecondContainer().append(splitLayout);
+var mySVG=new SVGEditor();
+mySVG.setSize(wind.getWidth*0.25, wind.getHeight());
+
+// var holdingCell = [];
+
+//button on-click functionality
+andGate.whenClicked().then(
+    (button) => {
+        //holdingCell.push(new ANDGate("gates/noun-and-gate-5536388.svg", false));
+        // var andgate = new ANDGate("gates/noun-and-gate-5536388.svg", false);
+        // console.log(andgate.getSVGContent());
+        mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-and-gate-5536388.svg" ,preserveAspectRatio:"none"});
+    }
+);
+orGate.whenClicked().then(
+    (button) => {
+        //var gate2 = new ORGate("gates/noun-or-gate-5536390.svg", false); // debug console: "Uncaught ReferenceError ReferenceError: ORGate is not defined" idk why its saying that
+        mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-or-gate-5536390.svg" ,preserveAspectRatio:"none"});
+    }
+);
+notGate.whenClicked().then(
+    (button) => {
+        // init gate class
+        mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-not-gate-5536385.svg",preserveAspectRatio:"none"});
+    }
+);
+xorGate.whenClicked().then(
+    (button) => {
+        // init gate class
+        mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-xor-gate-5536387.svg",preserveAspectRatio:"none"});
+    }
+);
+nandGate.whenClicked().then(
+    (button) => {
+        // init gate class
+        mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-nand-gate-1375339.svg",preserveAspectRatio:"none"});
+    }
+);
+norGate.whenClicked().then(
+    (button) => {
+        // init gate class
+        mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-nor-77568.svg",preserveAspectRatio:"none"});
+    }
+);
+xnorGate.whenClicked().then(
+    (button) => {
+        // init gate class
+        mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-xnor-gate-3834994.svg",preserveAspectRatio:"none"});
+    }
+);
+inputSignal.whenClicked().then(
+    (button) => {
+        // init gate class
+        mySVG.image({y:50,x:50,width:50,height:50,href:"gates/line-with-dot.svg",preserveAspectRatio:"none"});
+    }
+);
+connectorSignal.whenClicked().then(
+    (button) => {
+        // init gate class
+        mySVG.image({y:50,x:50,width:50,height:50,href:"gates/noun-line-924522.svg",preserveAspectRatio:"none"});
+    }
+);
+outputSignal.whenClicked().then(
+    (button) => {
+        // init gate class
+        mySVG.image({y:50,x:50,width:50,height:50,href:"gates/flipped-line-with-dot.svg",preserveAspectRatio:"none"});
+    }
+);
+
+div.appendChild(mySVG.svg);
+
+menulayout.getContainer().div.scrollTop=mySVG.getHeight()/2-wind.getHeight()/2;
+menulayout.getContainer().div.scrollLeft=mySVG.getWidth()/2-wind.getWidth()/2;
+
 }
-   
-    // const svgns = "http://www.w3.org/2000/svg";
-    // //link for assistance creating dynamic svg elements -> https://www.motiontricks.com/creating-dynamic-svg-elements-with-javascript/
-    // let svg=document.createElementNS(svgns, "svg");
-    // svg.setAttribute("width","100%");
-    // svg.setAttribute("height","100%");
-
-    //svg.setAttribute("viewBox","0 0 2000 2000");
-    //area.appendChild(svg);
-    
-    //end
 
 
 
-    //only need to change SVGEditor, but everything can be draggable
-    //start with rect only
+//end
 
-    /* This is an example of the SVGEditor class
-    var mySVG=new SVGEditor();
-	mySVG.setSize(3000,3000);
-    mySVG.rect({y:1320,x:1500,width:200,height:200,stroke:'#000000',fill:"rgba(0,0,0,0)",strokeWidth:"3"},{canResize:true})
-    mySVG.rect({y:1310,x:1310,width:20,height:20,stroke:'#000000',fill:"rgba(0,0,0,0)",strokeWidth:"3"})
-    mySVG.ellipse({cy:1400,cx:1310,rx:209,ry:20,stroke:'#000000',fill:"rgba(0,0,0,0)",strokeWidth:"3"})
-    mySVG.text({x:1350,y:1350,text:"Hello this is my test",'font-family':'Arial','font-size':'20px',fill:'black',style:'user-select:none;'})
-	mySVG .image({y:1320,x:1500,width:200,height:200,href:"https://digitalworlds.github.io/op.n/img/logo.png",preserveAspectRatio:"none"});
 
-    div.appendChild(mySVG.svg);
-    
-	menulayout.getContainer().div.scrollTop=mySVG.getHeight()/2-wind.getHeight()/2;
-	menulayout.getContainer().div.scrollLeft=mySVG.getWidth()/2-wind.getWidth()/2;
-    */
+
+//only need to change SVGEditor, but everything can be draggable
+//start with rect only
+
+/* This is an example of the SVGEditor class
+var mySVG=new SVGEditor();
+mySVG.setSize(3000,3000);
+mySVG.rect({y:1320,x:1500,width:200,height:200,stroke:'#000000',fill:"rgba(0,0,0,0)",strokeWidth:"3"},{canResize:true})
+mySVG.rect({y:1310,x:1310,width:20,height:20,stroke:'#000000',fill:"rgba(0,0,0,0)",strokeWidth:"3"})
+mySVG.ellipse({cy:1400,cx:1310,rx:209,ry:20,stroke:'#000000',fill:"rgba(0,0,0,0)",strokeWidth:"3"})
+mySVG.text({x:1350,y:1350,text:"Hello this is my test",'font-family':'Arial','font-size':'20px',fill:'black',style:'user-select:none;'})
+mySVG .image({y:1320,x:1500,width:200,height:200,href:"https://digitalworlds.github.io/op.n/img/logo.png",preserveAspectRatio:"none"});
+
+div.appendChild(mySVG.svg);
+
+menulayout.getContainer().div.scrollTop=mySVG.getHeight()/2-wind.getHeight()/2;
+menulayout.getContainer().div.scrollLeft=mySVG.getWidth()/2-wind.getWidth()/2;
+*/
